b9b72c53a8e58251dff920f6455c128321826e47
Reynaldi Rukmana <naldieyy@gmail.com>
2026-06-30 00:52:33 +0700

    Fix frontend navbar for Railway

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,13 +1,31 @@
-<nav style="background:#19376D;padding:15px;">
-    <span style="color:white;font-weight:bold;">EduModule</span>
+<nav style="background:#19376D;padding:15px 40px;display:flex;justify-content:space-between;align-items:center;">
 
     <a href="{{ route('home') }}"
-       style="color:red;font-size:20px;margin-left:40px;">
-        HOME
+       style="color:white;
+              text-decoration:none;
+              font-size:28px;
+              font-weight:bold;">
+        📚 EduModule
     </a>
 
-    <a href="{{ route('home') }}#tentang"
-       style="color:yellow;font-size:20px;margin-left:20px;">
-        ABOUT
-    </a>
+    <div style="display:flex;gap:30px;">
+
+        <a href="{{ route('home') }}"
+           style="color:white;
+                  text-decoration:none;
+                  font-size:18px;
+                  font-weight:500;">
+            Home
+        </a>
+
+        <a href="{{ route('home') }}#tentang"
+           style="color:white;
+                  text-decoration:none;
+                  font-size:18px;
+                  font-weight:500;">
+            About
+        </a>
+
+    </div>
+
 </nav>
