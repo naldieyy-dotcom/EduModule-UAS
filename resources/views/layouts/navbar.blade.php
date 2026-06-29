0bcd16194b6b1e475ad4b19296fbf8b0cde2184a
Reynaldi Rukmana <naldieyy@gmail.com>
2026-06-30 00:48:43 +0700

    Test inline navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,19 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-navbar shadow">
-    <div class="container">
+<nav style="background:#19376D;padding:15px;">
+    <span style="color:white;font-weight:bold;">EduModule</span>
 
-        <a class="navbar-brand fw-bold" href="{{ route('home') }}">
-            <i class="bi bi-book-half"></i> EduModule
-        </a>
+    <a href="{{ route('home') }}"
+       style="color:red;font-size:20px;margin-left:40px;">
+        HOME
+    </a>
 
-        <ul class="navbar-nav ms-auto flex-row">
-            <li class="nav-item me-3">
-                <a class="nav-link text-white" href="{{ route('home') }}">Home</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link text-white" href="{{ route('home') }}#tentang">About</a>
-            </li>
-        </ul>
-
-    </div>
+    <a href="{{ route('home') }}#tentang"
+       style="color:yellow;font-size:20px;margin-left:20px;">
+        ABOUT
+    </a>
 </nav>
